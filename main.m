81ec160349b756d40848e058a79d52dab7f5440c
MaclaurinSeries <63801107+MaclaurinSeries@users.noreply.github.com>
2021-05-01 13:52:07 +0700

    gambar rubiknya udh di erode dan diatur hsv nya

--- main.m
+++ main.m
@@ -1,39 +1,33 @@
-size = [180 180];
-bit = 4;
-toleration = 10;
+clear all;
+size = [240 240];
 
-power = (2^bit - 1) * 1.0;
-img = cropRubik('gambar1.jpg', size);
-img = (floor(img * power)) / power;
-figure;imshow(img);
+img = cropRubik('referensi.jpg', size);
+figure; imshow(img);
 
-sizec = round(size / 3);
-img_potongan = zeros(3, 3);
 
-color_collection = zeros(6);
 %   1      2      3      4      5      6
 % merah, orange, kuning, hijau, biru, putih
 % hue (0, 40, 60, 120, 200), saturation 0%
 
-figure;
-for i=1:3
-    for j=1:3
-        rect = [(j-1)*(sizec(2)+toleration), (i-1)*(sizec(1)+toleration), sizec(2)-2*toleration, sizec(1)-2*toleration];
-        cropped = rgb2hsv(imcrop(img, rect));
-        domH = mode(mode(round(cropped(:,:,1) * 18.0) / 18.0));
-        domS = mode(mode(round(cropped(:,:,2) * 4.0) / 4.0));
-        domV = mode(mode(round(cropped(:,:,3) * 4.0) / 4.0));
-        
-        temp = zeros(1,1,3);
-        temp(1,1,1) = domH;
-        temp(1,1,2) = domS > 0.4;
-        temp(1,1,3) = domV > 0;
-        
-        subplot(3, 3, 3*(i-1)+j);
-        imshow(hsv2rgb(temp));
-    end
-end
-
-function insertColor(color, collection)
-    
-end
+% figure;
+% for i=1:3
+%     for j=1:3
+%         rect = [(j-1)*(sizec(2)+toleration), (i-1)*(sizec(1)+toleration), sizec(2)-2*toleration, sizec(1)-2*toleration];
+%         cropped = rgb2hsv(imcrop(img, rect));
+%         domH = mode(mode(round(cropped(:,:,1) * 18.0) / 18.0));
+%         domS = mode(mode(round(cropped(:,:,2) * 4.0) / 4.0));
+%         domV = mode(mode(round(cropped(:,:,3) * 4.0) / 4.0));
+%         
+%         temp = zeros(1,1,3);
+%         temp(1,1,1) = domH;
+%         temp(1,1,2) = domS > 0.4;
+%         temp(1,1,3) = domV > 0;
+%         
+%         subplot(3, 3, 3*(i-1)+j);
+%         imshow(hsv2rgb(temp));
+%     end
+% end
+% 
+% function insertColor(color, collection)
+%     
+% end
